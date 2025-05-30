2c42af600957eef5a5e8d695377cf5b24e4ea845
Vedant Upadhyay <vedantmahakal@gmail.com>
2025-05-30 19:50:09 +0530

    initial basic commit

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,12 +1,10 @@
-// Immediately Invoked Function Expressions (IIFE)
+// Immediately Invoked Function Expressions (IIFE) : It is a function that runs as soon as it is defined.
 
-
-(function chai(){
-    // named IIFE
-    console.log(`DB CONNECTED`);
+(function chai() {
+  // named IIFE
+  console.log(`DB CONNECTED`);
 })();
 
-( (name) => {
-    console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
-
+((name) => {
+  console.log(`DB CONNECTED TWO ${name}`);
+})("hitesh");
---
+++ 03_basics/context.js
@@ -0,0 +1,7 @@
+// 1. How js run some program with example with different phases
+// -> Global Execution context
+// -> Memory Phase : Variables and functions are stored in memory (hoisting). No code is executed yet.
+// -> Execution Phase : Code runs line by line, using values in memory.
+// Repeast step 2-3 continuosly for each function
+
+// 2. Call Stack : Keeps track of which function is currently running.Functions are pushed onto the stack when called, and popped off when done.
